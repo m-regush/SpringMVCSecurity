2a46a506a9904df21b14c82d4fcddfa3de2973fa
m-regush <55323492+m-regush@users.noreply.github.com>
2019-11-26 18:00:11 +0300

    Add files via upload

--- src/main/java/springUser/model/User.java
+++ src/main/java/springUser/model/User.java
@@ -1,15 +1,17 @@
 package springUser.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 
 @Entity
 @Table(name = "user")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -49,8 +51,6 @@ public class User {
         this.userRoles = userRoles;
     }
 
-    public User(String name, String password, UserRole role) {
-    }
 
     public Long getId() {
         return id;
@@ -68,8 +68,38 @@ public class User {
         this.name = name;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return userRoles;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return name;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public void setPassword(String password) {
